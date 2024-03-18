845ffe61beb9124d0dab95ae739b97b1ab45fae2
alecapple <44648359+alecapple@users.noreply.github.com>
2024-03-18 14:57:57 -0700

    Wrong get_mfccs file was being used prior (merge/git issue)
    We used the wrong get_mfccs in our initial commit, and we changed it to use the correct get_mfccs file, which uses our own functions instead of designAuditoryFilterBank

--- get_mfccs.m
+++ get_mfccs.m
@@ -4,6 +4,12 @@ function mfccs = get_mfccs(audio_file, num_mel_coeffs, frame_size, ...
     % Read in the audio file
     [audio_in, fs] = audioread(audio_file);
 
+    % Apply FIR notch filter
+    % cutoff = 200 / (fs/2);
+    % bw = 100 / (fs/2);
+    % [b, a] = iirnotch(cutoff, bw);
+    % audio_in = filter(b, a, audio_in);
+
     % Split the audio signal into frames
     [S, ~, ~] = stft(audio_in, "Window", window, "FFTLength", frame_size, ...
         "OverlapLength", overlap_size, "FrequencyRange", "onesided");
