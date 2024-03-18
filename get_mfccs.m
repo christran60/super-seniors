0d15d021978afab875b908de27de330fd18741ca
Christopher Tran <65186360+christran60@users.noreply.github.com>
2024-03-18 14:54:07 -0700

    Update get_mfccs.m

--- get_mfccs.m
+++ get_mfccs.m
@@ -4,33 +4,17 @@ function mfccs = get_mfccs(audio_file, num_mel_coeffs, frame_size, ...
     % Read in the audio file
     [audio_in, fs] = audioread(audio_file);
 
-    % Apply FIR notch filter
-    % cutoff = 200 / (fs/2);
-    % bw = 100 / (fs/2);
-    % [b, a] = iirnotch(cutoff, bw);
-
-    audio_in = filter(b, a, audio_in);
-
     % Split the audio signal into frames
     [S, ~, ~] = stft(audio_in, "Window", window, "FFTLength", frame_size, ...
         "OverlapLength", overlap_size, "FrequencyRange", "onesided");
-
-    % Take the magnitude of the frames
     S = abs(S);
-    
-    % Generate the mel-spaced filterbank
-    [filter_bank, ~] = designAuditoryFilterBank(fs, 'FFTLength', frame_size, ...
-        'NumBands', num_mel_coeffs, "FrequencyRange", [0 fs/2]);
-    
-    %disp("sizes of mfcc:" + size(filter_bank) + ", "+size(S))
-
-    % Apply the filter bank to the frames to create the mel spectrogram
-    mel_spec = filter_bank * S;
-    
-    % Generate cepstral coefficients with the mel spectrogram
-    mfccs_all = cepstralCoefficients(mel_spec, "NumCoeffs", num_mel_coeffs);
-
-    % Remove the first MFCC (DC component)
-    mfccs = mfccs_all(:, 2:end);
+        % Compute Mel filterbank
+    m = melfb(num_mel_coeffs, frame_size, fs);
+ 
+    melSpectrum = m * S;
+    % Apply cepstrum by using DCT
+    mfccs = dct(melSpectrum);
+    mfccs = transpose(mfccs);
+    mfccs = mfccs(:, 2:end);
 
 end
